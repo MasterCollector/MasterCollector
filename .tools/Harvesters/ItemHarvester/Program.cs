3ea0f2ec3d121a4469b01fec8c7e1bf24edb8f58
Pr3vention <Pr3vention@gmail.com>
2020-04-18 15:57:28 -0500

    Harvesting items now significantly less resource intensive by writing directly to file instead of memory

--- .tools/Harvesters/ItemHarvester/Program.cs
+++ .tools/Harvesters/ItemHarvester/Program.cs
@@ -17,7 +17,6 @@ namespace ItemHarvester
         private static readonly string region = ConfigurationManager.AppSettings["region"];
         private static readonly string locale = ConfigurationManager.AppSettings["locale"];
         private static readonly string nameSpace = ConfigurationManager.AppSettings["namespace"];
-        private static readonly List<object> ItemDB = new List<object>();
         private static readonly int SleepTimeInSeconds = Convert.ToInt32(ConfigurationManager.AppSettings["SleepTimeInSeconds"]);
 
         private static void Main(string[] args)
@@ -35,56 +34,67 @@ namespace ItemHarvester
             // TODO:: need to add DB file loading/appending so we don't have to reprocess the entire DB every time
 
             int currentID = minItemID;
+            int itemsSaved = 0;
             var httpClient = APIClient.GetClient();
-            do
+            DateTime start = DateTime.Now;
+            using (FileStream fs = new FileStream("itemDB.json", FileMode.Create))
             {
-                if (httpClient == null)
+                using (StreamWriter sw = new StreamWriter(fs))
                 {
-                    Console.Out.WriteLine("HTTPClient could not be retrieved. Exiting...");
-                    Environment.Exit(0);
-                }
-                var response = httpClient.GetAsync(new Uri(BaseItemAPIEndpoint, $"{currentID}").AddParameter("region", region).AddParameter("locale", locale).AddParameter("namespace", nameSpace)).Result;
-                switch ((int)response.StatusCode)
-                {
-                    case 200:   // got a successful response from Blizzard
-                        JObject itemData = JObject.Parse(response.Content.ReadAsStringAsync().Result);
-                        if (TryProcessResponse(itemData, out IDictionary<string, object> data))
+                    sw.Write("{\"itemDB\":[");
+                    do
+                    {
+                        if (httpClient == null)
                         {
-                            /* Notes:
-                             * Blizzard, in their infinite wisdom, classifies everything with a quality, item class and subclass, but the values aren't always what you expect when seeing them ingame
-                             * For example: toys are typically classified as "miscellaneous/junk" despite being it really being a type of account-wide collectible item.
-                             *              pets are "miscellaneous/companion pets"
-                             *              mounts are "miscellaneous/mounts"
-                             */
-                            ItemDB.Add(data);
-                            Console.Out.WriteLine($"Item {data["id"]}: {data["name"]}");
+                            Console.Out.WriteLine("HTTPClient could not be retrieved. Exiting...");
+                            Environment.Exit(0);
                         }
-                        else
+                        var response = httpClient.GetAsync(new Uri(BaseItemAPIEndpoint, $"{currentID}").AddParameter("region", region).AddParameter("locale", locale).AddParameter("namespace", nameSpace)).Result;
+                        switch ((int)response.StatusCode)
                         {
-                            Console.Out.WriteLine($"Received a valid response but could not process data for item {currentID}.");
+                            case 200:   // got a successful response from Blizzard
+                                JObject itemData = JObject.Parse(response.Content.ReadAsStringAsync().Result);
+                                if (TryProcessResponse(itemData, out IDictionary<string, object> data))
+                                {
+                                    /* Notes:
+                                     * Blizzard, in their infinite wisdom, classifies everything with a quality, item class and subclass, but the values aren't always what you expect when seeing them ingame
+                                     * For example: toys are typically classified as "miscellaneous/junk" despite being it really being a type of account-wide collectible item.
+                                     *              pets are "miscellaneous/companion pets"
+                                     *              mounts are "miscellaneous/mounts"
+                                     */
+                                    sw.Write($"{(itemsSaved > 0 ? "," : string.Empty)}{JsonConvert.SerializeObject(data)}");
+                                    Console.Out.WriteLine($"Item {data["id"]}: {data["name"]}");
+                                    itemsSaved++;
+                                }
+                                else
+                                {
+                                    Console.Out.WriteLine($"Received a valid response but could not process data for item {currentID}.");
+                                }
+                                currentID++;
+                                break;
+
+                            case 404:   // no info for this ID, we're okay to move on the the next one
+                                currentID++;
+                                break;
+
+                            case 429:   // too many requests. We'll sleep here for a bit, dispose the client and then reopen it
+                                Console.Out.WriteLine("HTTP status TOO_MANY_REQUESTS (429) detected. Pausing and re-establishing connection...");
+                                System.Threading.Thread.Sleep(SleepTimeInSeconds * 1000);
+                                httpClient = APIClient.GetClient(dispose: true);
+                                break;
+
+                            default:
+                                Console.Out.WriteLine($"Unknown response code for quest {currentID}: {response.StatusCode}");
+                                currentID++;
+                                break;
                         }
-                        currentID++;
-                        break;
-
-                    case 404:   // no info for this ID, we're okay to move on the the next one
-                        currentID++;
-                        break;
-
-                    case 429:   // too many requests. We'll sleep here for a bit, dispose the client and then reopen it
-                        Console.Out.WriteLine("HTTP status TOO_MANY_REQUESTS (429) detected. Pausing and re-establishing connection...");
-                        System.Threading.Thread.Sleep(SleepTimeInSeconds * 1000);
-                        httpClient = APIClient.GetClient(dispose: true);
-                        break;
-
-                    default:
-                        Console.Out.WriteLine($"Unknown response code for quest {currentID}: {response.StatusCode}");
-                        currentID++;
-                        break;
+                    } while (currentID <= maxItemID);
+                    sw.Write("]}");
                 }
-            } while (currentID <= maxItemID);
-            Console.Out.WriteLine($"Successfully harvested {ItemDB.Count} items.");
-            var finalData = new { itemDB = ItemDB };
-            File.WriteAllText("itemDB.json", JsonConvert.SerializeObject(finalData));
+            }
+            Console.Out.WriteLine($"Successfully harvested {itemsSaved} items.");
+            DateTime end = DateTime.Now;
+            Console.Out.WriteLine($"ItemDB harvesting completed in {(end - start).Hours} hour(s), { (end - start).Minutes} minute(s), and { (end - start).Seconds} second(s)");
         }
 
         private static bool TryProcessResponse(JObject ItemData, out IDictionary<string, object> data)
