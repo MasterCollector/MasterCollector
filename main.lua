017fcf09feb0bcd9c9ab3e36bad8fec79cda58ce
Pr3vention <Pr3vention@gmail.com>
2020-07-05 17:24:09 -0500

    Added appearance harvesting method

--- main.lua
+++ main.lua
@@ -1,2 +1,35 @@
 local name, addonTable = ...
 MasterCollector = addonTable
+
+MasterCollector.harvest = function()
+	MCAppearanceHarvestData = {}
+	local TMogGetItemInfo = C_TransmogCollection.GetItemInfo
+	local items = {}
+	local modIDs = {
+	   0,	-- no mods
+	   1,	-- normal dungeon
+	   2,	-- heroic dungeon
+	   3,	-- normal raid
+	   4,	-- lfr raid
+	   5,	-- heroic raid
+	   6,	-- mythic raid
+	   22,	-- timewalking
+	   23	-- 'mythic dungeon'
+	}
+	for itemID=1,180784,1--175000,175100,1--1,178015,1
+	do
+	   local id,_,_,loc,_,classID,subclassID = GetItemInfoInstant(itemID)
+	   if id and (classID == 4 or (classID == 2 and (subclassID ~= 0 and subclassID ~= 11))) then
+		  for idx=1,#modIDs,1 do
+			 local appearanceID,sourceID = TMogGetItemInfo(itemID, modIDs[idx])
+			 if appearanceID then
+				if not items[itemID] then items[itemID] = {} end
+				table.insert(items[itemID], { modIDs[idx], appearanceID, sourceID})
+			 end
+		  end
+	   end
+	end
+	MCAppearanceHarvestData = items
+	print('Appearance data has been collected. /reload or close your client and inspect SavedVariables for the collected data.')
+end
+
