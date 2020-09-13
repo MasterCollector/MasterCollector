c9a07590c234a80ab48a0afce911b2722fc8677c
Pr3vention <Pr3vention@gmail.com>
2020-09-12 19:19:46 -0500

    Added early module registry and event handler system
    Allows for plugins to the MasterCollector core engine

--- main.lua
+++ main.lua
@@ -1,5 +1,8 @@
-local name, addonTable = ...
-MasterCollector = addonTable
+local addonName = select(1,...)
+MasterCollector = select(2,...)
+local data = {
+	
+}
 
 MasterCollector.harvest = function()
 	MCAppearanceHarvestData = {}
@@ -33,3 +36,67 @@ MasterCollector.harvest = function()
 	print('Appearance data has been collected. /reload or close your client and inspect SavedVariables for the collected data.')
 end
 
+
+
+--------------------
+-- Event Handling --
+--------------------
+-- Events must be registered to a frame. Since we only want these to fire once, we'll create an invisible
+-- dummy frame to intercept and handle collection events
+local eventFrame = CreateFrame("FRAME", "MasterCollectorEventFrame", UIParent)
+eventFrame.events = {}
+eventFrame:RegisterEvent("VARIABLES_LOADED")
+eventFrame:RegisterEvent("ADDON_LOADED")
+eventFrame:SetScript("OnEvent", function(self, event, ...)
+	if eventFrame.events[event] then
+		-- each event can have multiple handlers due to modules, so we want to fire each handler
+		if type(eventFrame.events[event]) == 'table' then
+			for k,v in pairs(eventFrame.events[event]) do v(...) end
+		else
+			eventFrame.events[event](...)
+		end
+	end
+end)
+eventFrame.events.VARIABLES_LOADED = function()
+	print('MASTERCOLLECTOR VARIABLES_LOADED')
+end
+eventFrame.events.ADDON_LOADED = function(loadedAddonName)
+	if loadedAddonName:find('^'..addonName..'%-') then
+		print('MASTERCOLLECTOR: ' .. loadedAddonName)
+	end
+end
+function MasterCollector:UnregisterEvent(event, func)
+	if eventFrame.events[event] then
+		if type(eventFrame.events[event]) == 'table' then
+			for k,v in pairs(eventFrame.events[event]) do
+				if v == func then
+					eventFrame.events[event][k]=nil
+					print('successfully unregistered tabled event ' .. event)
+					return
+				end
+			end
+		else
+			eventFrame.events[event]=nil
+			print('successfully unregistered single event ' .. event)
+		end
+	end
+end
+function MasterCollector:RegisterModule(ModuleName, mod)
+	if MasterCollector.Modules[ModuleName] then
+		print('Module "'..ModuleName..'" cannot be registered twice.')
+		return
+	end
+	MasterCollector.Modules[ModuleName]={
+		DB = mod.DB
+	}
+	if mod.events then
+		for k,v in pairs(mod.events) do
+			if not eventFrame.events[k] then
+				eventFrame.events[k] = {}
+				eventFrame:RegisterEvent(k)
+			end
+			table.insert(eventFrame.events[k], v)
+		end
+	end
+	print(ModuleName .. ' registered successfully')
+end
