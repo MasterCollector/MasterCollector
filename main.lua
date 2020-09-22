231b611f4075d8ab051e717b2ecf9998ece8d96d
Pr3vention <Pr3vention@gmail.com>
2020-09-21 20:33:44 -0500

    Added initial window controls as a starting point for a more robust framework

--- main.lua
+++ main.lua
@@ -36,7 +36,6 @@ MasterCollector.harvest = function()
 	print('Appearance data has been collected. /reload or close your client and inspect SavedVariables for the collected data.')
 end
 
-
 ---------------------
 -- Window controls --
 ---------------------
@@ -44,8 +43,65 @@ end
 -- TODO: an actual window design rather than a simple box
 
 -- NOTE: NOT THE FINAL PRODUCT BUT SUFFICIENT FOR EARLY DRAFT TESTING OF CORE FUNCTIONALITY
-local FRAME_WIDTH = 300
-local FRAME_HEIGHT = 500
+local WINDOW_MIN_WIDTH = 300
+local WINDOW_MIN_HEIGHT = 200
+local WINDOW_WIDTH=300
+local WINDOW_HEIGHT=500
+local WINDOW_LEFT_MARGIN = 8
+local WINDOW_RIGHT_MARGIN = -8
+local ROW_HEIGHT = 16
+local ICON_WIDTH = 16
+local INDENT_LEVEL_SPACING = 16
+local SCROLLBAR_WIDTH = 18
+--[[
+local function CreateRow(anchor, previous, rowType, indent)
+    local row = CreateFrame("BUTTON", nil, anchor)
+    row:SetHeight(ROW_HEIGHT)
+    row:SetWidth(anchor:GetWidth())
+    if not previous then
+        row:SetPoint("TOPLEFT", anchor)
+    else
+        row:SetPoint("TOPLEFT", previous, "BOTTOMLEFT", INDENT_LEVEL_SPACING * indent, 0)
+    end
+    
+    local lbl = row:CreateFontString(nil, "ARTWORK", "GameFontNormal")
+    lbl:SetWidth(WINDOW_WIDTH)
+    lbl:SetHeight(ROW_HEIGHT)
+    lbl:SetPoint("TOPLEFT", row, "TOPLEFT", WINDOW_LEFT_MARGIN, 0)
+    lbl:SetJustifyH("LEFT")
+    lbl:SetText("DUMMY LABEL FOR " .. rowType .. ".")
+    
+    if rowType == "data" then
+        -- 16*indentLevel pixel space per indent level
+        -- 16 pixel collection status icon, left align
+        -- maybe a 16 pixel block for a row icon?
+        -- X pixel width content area
+    elseif rowType == "panel" then
+        row.Expandable = true
+        row.Expanded = true
+        
+        -- 16*indentLevel pixel space per indent level
+        local icon = row:CreateTexture(nil, "ARTWORK")
+        icon:SetSize(ICON_WIDTH, ICON_WIDTH)
+        icon:SetPoint("TOPRIGHT", row, "TOPRIGHT", WINDOW_RIGHT_MARGIN, 0)
+        icon:SetTexture("Interface\\AddOns\\MasterCollector\\assets\\Collapse") -- Expand.blp if the panel can be opened, Collapse.blp if it can be closed
+        -- X pixel width content area
+        -- 16 pixel panel expand/collapse icon, right align
+        icon:Show()
+        
+        row:RegisterForClicks("LeftButtonUp")
+        row:SetScript("OnClick", function(self, button)
+                row.Expanded = not row.Expanded
+                if row.Expanded then
+                    icon:SetTexture("Interface\\AddOns\\MasterCollector\\assets\\Collapse")
+                else
+                    icon:SetTexture("Interface\\AddOns\\MasterCollector\\assets\\Expand")
+                end
+        end)
+    end
+    row:Show()
+    return row
+end
 local function CloseCascadeFrame(targetFrame)
     if targetFrame then
         if targetFrame.cascadeWindow then
@@ -83,16 +139,17 @@ local function OpenCascadingWindow(anchorFrame)
             end
     end)
     anchorFrame:SetScript("OnLeave", function(s, motion)
-            -- If the frame the mouse goes to isn't the cascade frame, then we want to close the cascade frame instead
+            -- If the frame the mouse goes to isn't the new cascade frame, then we want to close it instead
+            -- TODO: make this work without depending on a name. It shouldn't be necessary since the anchor frame should know it has a child frame
             local frameUnderMouse = GetMouseFocus()
-            if anchorFrame.cascadeWindow and not frameUnderMouse:GetName():find('^MasterCollector.*CascadeFrame.*$') then
+            if not frameUnderMouse:GetName() or (anchorFrame.cascadeWindow and not frameUnderMouse:GetName():find('^MasterCollector.*CascadeFrame.*$')) then
                 CloseCascadeFrame(anchorFrame.cascadeWindow)
             end
     end)
     cascadeFrame:Show()
 end
 -- TODO: this function needs to be made more flexible so we can create windows on demand with unique names
-local CreateWindow = function(windowName)
+local CreateWindow = function(windowName, windowType)
     if _G[windowName] then
         _G[windowName]:Hide()
         _G[windowName] = nil
@@ -101,9 +158,13 @@ local CreateWindow = function(windowName)
     window:SetMovable(true)
     window:SetToplevel(true)
     window:EnableMouse(true)
-    window:SetPoint("CENTER") -- this line is responsible for the window position reseting on /reloadui
-    window:SetWidth(FRAME_WIDTH)
-    window:SetHeight(FRAME_HEIGHT)
+    window:SetPoint("RIGHT", -400, 0) -- this line is responsible for the window position reseting on /reloadui
+    window:SetWidth(WINDOW_WIDTH)
+    window:SetHeight(WINDOW_HEIGHT)
+    window:SetResizable(true)
+    window:SetMinResize(WINDOW_MIN_WIDTH, WINDOW_MIN_HEIGHT)
+    window:SetClampedToScreen(true) -- prevents window from being dragged off-screen
+    window:SetUserPlaced(true) -- this will allow window placement to be stored in cache, but is that reliable enough? Should we set up a window placement along with the saved sessions?
     
     local bd = {
         bgFile = "Interface\\DialogFrame\\UI-DialogBox-Background",
@@ -134,17 +195,54 @@ local CreateWindow = function(windowName)
     fnt:SetWidth(200)
     fnt:SetPoint("LEFT", window.titleLabel, "LEFT", 8, -2)
     fnt:SetJustifyH("LEFT")
-    local mapName = C_Map.GetMapInfo(C_Map.GetBestMapForUnit("player")).name or "UNKNOWN MAP"
-    fnt:SetText(mapName)
+    --local mapName = C_Map.GetMapInfo(C_Map.GetBestMapForUnit("player")).name or "UNKNOWN MAP"
+    fnt:SetText('Loading...')
     window.titleLabel:RegisterForClicks("RightButtonUp")
     window.titleLabel:SetScript("OnClick", function(s, button, down)
             if button == 'RightButton' then
                 OpenCascadingWindow(window.titleLabel)
             end
     end)
+    
+    local scrollFrame = CreateFrame('ScrollFrame', windowName .. 'scrollframe', window)
+    scrollFrame:SetPoint("TOPLEFT", window.titleLabel, "BOTTOMLEFT")
+    scrollFrame:SetPoint("BOTTOMRIGHT", window, "BOTTOMRIGHT")
+    
+    local grip = CreateFrame('Button', nil, scrollFrame)
+    grip:SetSize(16, 16)
+    grip:SetPoint("BOTTOMRIGHT", -5, 5)
+    grip:SetNormalTexture("Interface\\AddOns\\MasterCollector\\assets\\resize")
+    grip:RegisterForDrag("LeftButton")
+    grip:SetScript("OnDragStart", function() window:StartSizing() end)
+    grip:SetScript("OnDragStop", function() window:StopMovingOrSizing() end)
+    scrollFrame.grip = grip
+    
+    local scrollbar = CreateFrame('Slider', windowName .. 'scrollBar', scrollFrame, "UIPanelScrollBarTemplate")
+    scrollbar:SetWidth(SCROLLBAR_WIDTH)
+    scrollbar:SetPoint("TOPLEFT", scrollFrame, "TOPRIGHT", -SCROLLBAR_WIDTH, -16)
+    scrollbar:SetPoint("BOTTOMRIGHT", grip, "TOPRIGHT", 0, 16)
+    scrollbar:SetMinMaxValues(0, 10)
+    scrollbar:SetValue(0)
+    scrollbar:SetValueStep(1)
+    scrollFrame.bar = scrollbar
+    
+    local dataArea = CreateFrame('FRAME', windowName .. 'dataArea', scrollFrame)
+    dataArea:SetPoint("TOPLEFT", window.titleLabel, "BOTTOMLEFT")
+    dataArea:SetPoint("BOTTOMRIGHT", scrollbar, "BOTTOMLEFT")
+    window.dataArea = dataArea
+    
+    
+    -- Map events based on the type of window
+    if windowType == "currentZone" then
+        window:RegisterEvent("ZONE_CHANGED_NEW_AREA")
+        window:SetScript("OnEvent", function(self, event, ...)
+                fnt:SetText(C_Map.GetMapInfo(C_Map.GetBestMapForUnit("player")).name or "UNKNOWN MAP")
+        end)
+    end
 end
 -- TODO: creating the window before the player finishes loading into the world will cause an error if setting the titleLabel with the current zone text
-CreateWindow("MasterCollectorCurrentZone")
+CreateWindow("MasterCollectorCurrentZone", "currentZone")
+]]--
 
 
 --------------------
