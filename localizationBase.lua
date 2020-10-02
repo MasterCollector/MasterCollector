39d65a6dfa2abd8dfdf932978f148cc9c32f643d
Pr3vention <Pr3vention@gmail.com>
2020-10-01 22:57:27 -0500

    Added a base file to start with localization

---
+++ localizationBase.lua
@@ -0,0 +1,10 @@
+MasterCollector = select(2,...)
+MasterCollector.L = {}
+
+-- panel info
+MasterCollector.L.Panels = {
+	[1] = {
+		text = AUCTION_CATEGORY_BATTLE_PETS,
+		icon = "Interface\\Icons\\Tracking_WildPet",
+	},
+}
