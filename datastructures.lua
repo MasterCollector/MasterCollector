f40a37338370dc94050aecea1fd3038717a50ad7
Pr3vention <Pr3vention@gmail.com>
2020-10-02 16:42:32 -0500

    Row and Panel visibility are now calculated

--- datastructures.lua
+++ datastructures.lua
@@ -1,6 +1,30 @@
 local addonTable = select(2, ...)
 local L = addonTable.L
 local GetPetInfoBySpeciesID = C_PetJournal.GetPetInfoBySpeciesID
+
+-- supporting functions for the data structure metatables
+local function determineVisibility(tbl)
+	if tbl.type == "panel" then
+		-- if the panel has no children (should never happen but this is a safety check), don't show it
+		if not tbl.children then return false end
+		-- check all children of the panel. If any of them are visible, then the panel itself must be visible
+		for _,v in pairs(tbl.children) do
+			if determineVisibility(v) then return true end
+		end
+		-- if no children are visible, then the panel doesn't need to be shown
+		return false
+	else
+		local optionShowCollected = false -- TODO: replace with addon setting when the settings panel is written
+		-- if the object has already been collected and the user has chosen not to show collected items, then don't show the object
+		if tbl.collected and not optionShowCollected then
+			return false
+		end
+	end
+	
+	return true
+end
+
+-- All metatables describing data structures should be added below
 addonTable.structs = {}
 addonTable.structs.panel = {
 	__index = function(self, key)
@@ -11,7 +35,7 @@ addonTable.structs.panel = {
 		elseif key == "type" then
 			return "panel"
 		elseif key == "visible" then
-			return true -- temporarily hard-coding visibility
+			return determineVisibility(self)
 		else
 			return rawget(self, key)
 		end
@@ -27,7 +51,7 @@ addonTable.structs.pet = {
 		elseif key == "type" then
 			return "data"
 		elseif key == "visible" then
-			return true -- temporarily hard-coding visibility
+			return determineVisibility(self)
 		else
 			return nil
 		end
