b0b0c67d1bf2e638a3f6381c684de1c4811d1104
Pr3vention <Pr3vention@gmail.com>
2020-10-03 14:17:20 -0500

    Added initial setup files for quest module

---
+++ modules/quests/data.lua
@@ -0,0 +1,9 @@
+-- TODO: check if quests module is enabled. If not, return immediately
+--if true then return end
+local MasterCollector = select(2,...)
+MasterCollector.Modules.Quests = {}
+MasterCollector.Modules.Quests.DB={
+}
+MasterCollector.Modules.Quests.mapData={
+	[461]={{"quest",{25130,25132,25126,25136,25128,25172,37446,25133,25129,25127,25135,25131,25152}}},
+}
---
+++ modules/quests/module.lua
@@ -0,0 +1,11 @@
+local MasterCollector = select(2,...)
+local mod = MasterCollector.Modules.Quests
+if mod then
+	-- scan through mapData and initialize the known objects by key
+	MasterCollector:InitializeDatabases(mod)
+	-- Set up all the events that this module will use
+	local events = {}
+	-- Last step is to register the mod-specific events to the core engine
+	MasterCollector:RegisterModuleEvents(events)
+	MasterCollector:FlagModAsLoaded("Quests")
+end
