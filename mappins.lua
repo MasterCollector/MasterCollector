7db7654dfb3c4adcbd2270e0ef919d5126a9155f
Pr3vention <Pr3vention@gmail.com>
2021-02-11 21:44:51 -0600

    Fixed some pinFramePool management issues and duplicate pin mapping

--- mappins.lua
+++ mappins.lua
@@ -13,32 +13,47 @@ function MapPins:TryMapObject(obj)
 	local coord
 	for i=1, #obj.coordinates do
 		coord = obj.coordinates[1]
+		if not coord.wp then
 		-- try to get a frame from the pool first before creating new ones
-		local point = table.remove(pinFramePool)
-		
-		if not point then
-			point = {}
-			local mm = CreateFrame("BUTTON", nil, minimapParent)
-			mm:SetHeight(16)
-			mm:SetWidth(16)
-			mm.icon = mm:CreateTexture(nil, "OVERLAY")
-			mm.icon:SetPoint("CENTER", 0, 0)
-			mm.icon:SetBlendMode("BLEND")
-			point.mm = mm
+			local point = table.remove(pinFramePool)
+			
+			if not point then
+				point = {}
+				local mm = CreateFrame("BUTTON", nil, minimapParent)
+				mm:SetHeight(16)
+				mm:SetWidth(16)
+				mm.icon = mm:CreateTexture(nil, "OVERLAY")
+				mm.icon:SetPoint("CENTER", 0, 0)
+				mm.icon:SetBlendMode("BLEND")
+				point.mm = mm
+			end
+			
+			point.x = coord[1] / 100.00
+			point.y = coord[2] / 100.00
+			point.mapID = coord[3]
+			coord.wp = point
+			
+			if obj.icon then
+				point.mm.icon:SetTexture(obj.icon)
+			else 
+				point.mm.icon:SetTexture("Interface\\minimap\\objecticons")
+				point.mm.icon:SetTexCoord(0.875, 1, 0, 0.125)
+			end
+			point.mm.icon:SetHeight(16)
+			point.mm.icon:SetWidth(16)
+			self:SetPin(point)
 		end
-		
-		point.x = coord[1] / 100.00
-		point.y = coord[2] / 100.00
-		point.mapID = coord[3]
-		if obj.icon then
-			point.mm.icon:SetTexture(obj.icon)
-		else 
-			point.mm.icon:SetTexture("Interface\\minimap\\objecticons")
-			point.mm.icon:SetTexCoord(0.875, 1, 0, 0.125)
+	end
+end
+function MapPins:TryRemoveObjectPins(obj)
+	if not obj or not obj.coordinates then return end
+	local coord
+	for i=1, #obj.coordinates do
+		coord = obj.coordinates[1]
+		if coord.wp then
+			MapPins:RemovePin(coord.wp)
+			coord.wp = nil
 		end
-		point.mm.icon:SetHeight(16)
-		point.mm.icon:SetWidth(16)
-		self:SetPin(point)
 	end
 end
 
@@ -47,6 +62,6 @@ function MapPins:SetPin(point)
 	hbdp:AddMinimapIconMap(self, point.mm, point.mapID, point.x, point.y, true, true)
 end
 function MapPins:RemovePin(point)
-	pool.insert(point)
 	hbdp:RemoveMinimapIcon(self, point.mm)
+	table.insert(pinFramePool, point)
 end
