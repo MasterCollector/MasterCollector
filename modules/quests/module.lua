a230c7a192cde4b27f157b7b3354ef5598456746
Pr3vention <Pr3vention@gmail.com>
2021-02-13 23:06:51 -0600

    Quest completion now auto-removes the related map pins
    Works fine for objects in the quest module but not the ideal solution for non-quest objects. This can probably be moved to the modManager for wider support of event hooks

--- modules/quests/module.lua
+++ modules/quests/module.lua
@@ -1,4 +1,5 @@
 local MasterCollector = select(2,...)
+local Mappins = MasterCollector.MapPins
 local mod = MasterCollector.Modules.Quests
 if mod then
 	-- scan through mapData and initialize the known objects by key
@@ -6,8 +7,16 @@ if mod then
 	-- Set up all the events that this module will use
 	local events = {}
 	events.QUEST_TURNED_IN = function(questID)
-		if mod.DB.quest[questID] then
-			mod.DB.quest[questID].collected = true
+		local quest = mod.DB.quest[questID]
+		if quest then
+			quest.collected = true
+			if quest.coordinates then
+				for i=1,#quest.coordinates do
+					if quest.coordinates[i].wp then 
+						Mappins:RemovePin(quest.coordinates[i].wp)
+					end
+				end
+			end
 			MasterCollector:RefreshWindows()
 		end
 	end
