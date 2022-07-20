90380ef1a9e1c1e649d5575db06e80299295523b
Pr3vention <12611124+Pr3vention@users.noreply.github.com>
2022-07-19 19:59:52 -0500

    Data snapshot update

--- modules/treasures/data.lua
+++ modules/treasures/data.lua
@@ -1,8 +1,16 @@
 -- TODO: check if treasures module is enabled. If not, return immediately
 --if true then return end
 local MasterCollector = select(2,...)
-MasterCollector.Modules.Treasures = {}
+--@GENERATE_HERE@--
+MasterCollector.Modules.Treasures={}
 MasterCollector.Modules.Treasures.DB={
+	["quest"]={
+		[56088]={coordinates={{41.84,42.84,1409}},providers={{"o",327407}},requirements={quest={55981,59978}}},
+		[56579]={coordinates={{58.47,59.33,1409}},providers={{"o",329918}}},
+		[56581]={coordinates={{59.36,37.69,1409}},providers={{"o",329919}}},
+		[58380]={coordinates={{57.08,68.43,1409}},providers={{"o",339770}}},
+	},
 }
 MasterCollector.Modules.Treasures.mapData={
+	[1409]={{"quest",{56088,56579,56581,58380}}},
 }
