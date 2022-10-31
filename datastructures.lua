13b2d5d7c66cad6ebf9c5c9cf96ab08fe27e3e3c
Pr3vention <12611124+Pr3vention@users.noreply.github.com>
2022-10-30 20:33:33 -0500

    battle pet items now show their collected state

--- datastructures.lua
+++ datastructures.lua
@@ -230,6 +230,8 @@ MasterCollector.structs.item = {
 		elseif key == "collected" then
 			if self.type == 'equipment' then
 				return C_TransmogCollection.PlayerHasTransmog(self.id)
+			elseif self.type == 'pet' then
+				return C_PetJournal.GetNumCollectedInfo(self.speciesID) > 0
 			elseif self.type == 'toy' then
 				return PlayerHasToy(self.id)
 			end
