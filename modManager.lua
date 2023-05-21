8eb4c0f6fbef32bea027712e602b55e2434e021c
Pr3vention <12611124+Pr3vention@users.noreply.github.com>
2023-05-21 00:03:58 -0500

    More cleanup of old event strategy

--- modManager.lua
+++ modManager.lua
@@ -6,21 +6,19 @@ local C_PetJournal = C_PetJournal
 --------------------
 -- Event Handling --
 --------------------
+local events = {}
 local eventFrame = CreateFrame("FRAME", "MasterCollectorEventFrame", UIParent)
-eventFrame.events = {}
-eventFrame:RegisterEvent("ADDON_LOADED")
 eventFrame:SetScript("OnEvent", function(self, event, ...)
-	if eventFrame.events[event] then
-		-- each event can have multiple handlers due to modules, so we want to fire each handler
-		if type(eventFrame.events[event]) == 'table' then
-			for k,v in pairs(eventFrame.events[event]) do v(...) end
-		else
-			eventFrame.events[event](...)
-		end
+	if events[event] then
+		events[event](...)
 	end
 end)
+local function UnregisterEvent(event)
+	if events[event] then
+		eventFrame:UnregisterEvent(event)
+	end
+end
 
-local events = {}
 local numPets
 events.PET_JOURNAL_LIST_UPDATE = function ()
 	local newPetCount = select(2, C_PetJournal.GetNumPets())
@@ -32,8 +30,6 @@ events.PET_JOURNAL_LIST_UPDATE = function ()
 	end
 	MasterCollector:RefreshWindows()
 end
-
-
 events.ACHIEVEMENT_EARNED = function(achievementID, alreadyEarned)
 	-- TODO: where does 'alreadyEarned' come into play? Is it for achievements unlocked
 	--		 on your account but not on the character?
@@ -71,8 +67,7 @@ events.QUEST_ACCEPTED = function()
 	C_Timer.After(0.1, function() MasterCollector:RefreshWindows() end)
 end
 events.QUEST_REMOVED = events.QUEST_ACCEPTED
-
-eventFrame.events.ADDON_LOADED = function(loadedAddonName)
+events.ADDON_LOADED = function(loadedAddonName)
 	if loadedAddonName == addonName then
 		MasterCollector.playerData.class = select(3, UnitClass("player"))
 		MasterCollector.playerData.race = select(3, UnitRace("player"))
@@ -101,39 +96,9 @@ eventFrame.events.ADDON_LOADED = function(loadedAddonName)
 		end
 		
 		MasterCollector:Start()
-		-- we don't need to listen to this event anymore. Free up the memory of this function and unregister it from the frame listener
-		eventFrame.events.ADDON_LOADED = nil
-		eventFrame:UnregisterEvent('ADDON_LOADED')
-	end
-end
-function MasterCollector:UnregisterEvent(event, func)
-	if eventFrame.events[event] then
-		if type(eventFrame.events[event]) == 'table' then
-			for k,v in pairs(eventFrame.events[event]) do
-				if v == func then
-					eventFrame.events[event][k]=nil
-					return
-				end
-			end
-		else
-			eventFrame.events[event]=nil
-		end
-	end
-end
-function MasterCollector:RegisterModuleEvents(events)
-	for k,v in pairs(events or {}) do
-		if not eventFrame.events[k] then
-			eventFrame.events[k] = {}
-			eventFrame:RegisterEvent(k)
-		end
-		table.insert(eventFrame.events[k],v)
-	end
-end
-function MasterCollector:FlagModAsLoaded(modName)
-	if MasterCollector.Modules and MasterCollector.Modules[modName] then
-		MasterCollector.Modules[modName].loaded = true
 	end
 end
 
--- work in progress. Registing events at the addon-level doesn't make sense but there are too many dependencies right now to eliminate it
-MasterCollector:RegisterModuleEvents(events)
+for k,v in pairs(events or {}) do
+	eventFrame:RegisterEvent(k)
+end
