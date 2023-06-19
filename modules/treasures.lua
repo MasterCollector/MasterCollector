ca0dd43b2916e633fb806ba091a81e219998e25d
Pr3vention <12611124+Pr3vention@users.noreply.github.com>
2023-06-18 19:50:55 -0500

    Recompiled database

--- modules/treasures.lua
+++ modules/treasures.lua
@@ -1,5 +1,5 @@
 -- TODO: check if treasures module is enabled. If not, return immediately
-if true then return end
+--if true then return end
 local MasterCollector = select(2,...)
 --@GENERATE_HERE@--
 MasterCollector.Modules.Treasures={
